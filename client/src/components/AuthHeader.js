53131f06ebbeb3349fa83ede570dec2eb42f744e
Bhaski47 <bhaskarg2003@gmail.com>
2024-02-08 01:21:33 +0530

    Initialization

--- client/src/components/AuthHeader.js
+++ client/src/components/AuthHeader.js
@@ -4,10 +4,15 @@ import styles from '../styles/components/AuthHeader.module.css';
 function AuthHeader() {
   return (
     <div className={styles.navContainer}>
-      <div></div>
-      <div className={styles.userInfo}>
-        <h3 className={styles.userName}>Bhaskar</h3>
-        <button className={styles.button}>Sign Out</button>
+      <div className={styles.navBar}>
+        <div></div>
+        <div className={styles.userInfo}>
+          <div></div>
+          <h3 className={styles.userName}>Bhaskar</h3>
+          <button className={`${styles.button} ${styles.type1}`}>
+            <span class="btn-txt">Hello</span>
+          </button>
+        </div>
       </div>
     </div>
   )
