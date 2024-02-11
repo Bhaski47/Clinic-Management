2c44479d6d75608ec423136c6de91a10f40ec9a3
Bhaski47 <bhaskarg2003@gmail.com>
2024-02-11 12:03:00 +0530

    Added Changes To Server Side Files

--- server/controllers/createDoc.js
+++ server/controllers/createDoc.js
@@ -8,7 +8,7 @@ const createDoc = async (req, res) => {
         //     return res.status(400).send({ message: error.details[0].message });
         const doc = await Doc.findOne({ email: req.body.email });
         if (doc) return res.status(409).send({ message: "Already Registered" });
-        const salt = await bcrypt.genSalt(Number(10));
+        const salt = await bcrypt.genSalt(10);
         const hashedPass = await bcrypt.hash(req.body.password, salt);
         await Doc({ ...req.body, password: hashedPass }).save();
         res.status(200).send({ message: "Doctor Account Created" });
--- server/controllers/createRecept.js
+++ server/controllers/createRecept.js
@@ -5,7 +5,7 @@ const createRecept = async (req, res) => {
     try {
         const recept = await Recept.findOne({ phno: req.body.phno });
         if (recept) return res.status(409).send({ message: "Already Registered" });
-        const salt = await bcrypt.genSalt(Number(10));
+        const salt = await bcrypt.genSalt(10);
         const hashedPass = await bcrypt.hash(req.body.password, salt);
         await Recept({ ...req.body, password: hashedPass }).save();
         res.status(200).send({ message: "Receptionist Account Created" });
