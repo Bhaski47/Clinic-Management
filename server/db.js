66b99f10da1b97e3c4ba2163670c5315711a8e94
BhaskarG <32745158+Bhaski47@users.noreply.github.com>
2024-02-11 11:59:07 +0530

    Updated Backend Files

--- server/db.js
+++ server/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async() =>{
     try {
-        await mongoose.connect("mongodb+srv://bhaskarg2003:12345678@system.8j33bfu.mongodb.net/?retryWrites=true&w=majority");
+        await mongoose.connect("mongodb+srv://bhaskarg2003:7904610528ig@system.8j33bfu.mongodb.net/?retryWrites=true&w=majority");
         console.log("Connected To DB");
     } catch (error) {
         console.error('Error Connecting To DB '+error);
