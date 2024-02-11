16222a7cca5d8bcbb2aab31771fd0e154bf3ceda
BhaskarG <32745158+Bhaski47@users.noreply.github.com>
2024-02-11 12:08:26 +0530

    Update createDoc.js

--- server/controllers/createDoc.js
+++ server/controllers/createDoc.js
@@ -3,17 +3,21 @@ const bcrypt = require('bcrypt');
 
 const createDoc = async (req, res) => {
     try {
-        // const { error } = validate(req.body);
-        // if (error)
-        //     return res.status(400).send({ message: error.details[0].message });
         const doc = await Doc.findOne({ email: req.body.email });
         if (doc) return res.status(409).send({ message: "Already Registered" });
+
+        // Generate a salt
         const salt = await bcrypt.genSalt(10);
+
+        // Hash the password using the generated salt
         const hashedPass = await bcrypt.hash(req.body.password, salt);
+
+        // Save the document with the hashed password
         await Doc({ ...req.body, password: hashedPass }).save();
+
         res.status(200).send({ message: "Doctor Account Created" });
     } catch (err) {
-        return res.status(500).send({ message: "Server Error For Creating Doctor " + err })
+        return res.status(500).send({ message: "Server Error For Creating Doctor " + err });
     }
 }
 
